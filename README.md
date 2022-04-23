1d4d21bdfaa556362884cd2f24ad23671d6c8df9
lampros chryssovergis <lampros chryssovergis>
2022-04-23 17:46:21 +0200

    adding .env, .gitignore and requirments.txt

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gitReport
+
+Requirments have been captured via 
+pip freeze > requirements.txt
+Please install neccessary requirments by running
+pip install -r requirements.txt
--- _gitReport.py
+++ _gitReport.py
@@ -1,14 +1,9 @@
 from textwrap import wrap
-import time
-import json
-import time
-import calendar
-import os
-
+from webbrowser import get
 from github import Github
 import xlsxwriter
-from dotenv import load_dotenv
-
+import time
+import json
 
 def get_xlsx_obj():
     # Create a workbook and add a worksheet.
@@ -17,29 +12,14 @@ def get_xlsx_obj():
 
     return workbook
 
-
 def save_and_close_xlsx_obj(workbook):
     # Save and close xlsx
     workbook.close()
-
-
-def sleep_if_core_rate_limit_reached(g):
-    core_rate_limit_remaining = g.get_rate_limit().core.remaining
-    core_rate_limit = g.get_rate_limit().core
-    reset_timestamp = calendar.timegm(core_rate_limit.reset.timetuple())
-    sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 5
-    if core_rate_limit_remaining < 10:
-        print(f"Core rate limit remaining ==> {core_rate_limit_remaining}")
-        print(f"Sleeping for {sleep_time}...")
-        print()
-        time.sleep(sleep_time)
-
 
 def get_orgs_list(g):
     orgslist = [orgs.login for orgs in g.get_user().get_orgs()]
         
     return orgslist
-
 
 def get_orgs_repo_details(g, o, xlsx):
     # create a worksheet
@@ -120,7 +100,6 @@ def get_orgs_repo_details(g, o, xlsx):
                     worksheet.write(row, 7, 'No prod')
                 row += 1
 
-
 def get_teams_repo_permissions(g, o, xlsx):
     # create a worksheet
     worksheet = xlsx.add_worksheet('Teams Permissions')
@@ -171,7 +150,6 @@ def get_teams_repo_permissions(g, o, xlsx):
 
                     row += 1
 
-
 def get_memebers_repo_permissions(g, o, xlsx):
     # create a worksheet
     worksheet = xlsx.add_worksheet('Members Permissions')
@@ -196,69 +174,44 @@ def get_memebers_repo_permissions(g, o, xlsx):
 
     worksheet.set_column('B:B', 30, fcenter)
     worksheet.set_column('C:C', 30, fcenter)
-    worksheet.set_column('D:D', 15, fcenter)
-    worksheet.set_column('E:E', 25, fwrap)
+    worksheet.set_column('D:D', 10, fcenter)
+    worksheet.set_column('E:E', 50, fwrap)
     
     # headers
     worksheet.write(1, 1, "Repo", fheaders)
     worksheet.write(1, 2, "Collaborator", fheaders)
     worksheet.write(1, 3, "Repo admin", fheaders)
-    worksheet.write(1, 4, "Permissions", fheaders)
+    worksheet.write(1, 3, "Permissions", fheaders)
 
     for org in g.get_user().get_orgs():
         if org.login == o:
             row = 2
             for repo in org.get_repos():
-                worksheet.write(row, 1, repo.name)
+                print(repo.name)
                 for collaborator in repo.get_collaborators():
-                    # Sleep if no more requests are available until they have been replenished
-                    print(f"{row}")
-                    sleep_if_core_rate_limit_reached(g)
-                    # permissions
-                    # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html
+                    # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.htmlgit@github.com:CLampros/gitReport.git
                     # https://docs.github.com/en/rest/reference/collaborators#get-repository-permissions-for-a-user
-                    # https://docs.github.com/en/enterprise-server@3.2/organizations/managing-access-to-your-organizations-repositories/viewing-people-with-access-to-your-repository
-                    json_permissions = json.dumps(collaborator.permissions.raw_data, indent = 4)
-                    json_permissions = json_permissions.replace('{', '')
-                    json_permissions = json_permissions.replace('}', '')
-                    worksheet.write(row, 2, collaborator.login)
-                    worksheet.write(row, 3, repo.get_collaborator_permission(collaborator))
-                    worksheet.write(row, 4, json_permissions)
-                    row += 1
-
+                    print(repo.get_collaborator_permission(collaborator))
 
 def main():
-    # Setup.
-    load_dotenv()
-    GIT_TOKEN = os.environ.get("GIT_TOKEN")
+    # setup
     hostname = "github.kyndryl.net"
     base_url = f"https://{hostname}/api/v3"
-    g = Github(base_url=base_url, login_or_token=GIT_TOKEN)
-
-    # core_rate_limit = g.get_rate_limit().core
-    # reset_timestamp = calendar.timegm(core_rate_limit.reset.timetuple())
-    # sleep_time = reset_timestamp - calendar.timegm(time.gmtime())
-    # print(f"Remaining searches ==> {g.get_rate_limit().search.remaining}")
-    # print(f"{core_rate_limit.remaining=}")
-    # print(f"{reset_timestamp=}")
-    # print(f"{sleep_time=}")
+    g = Github(base_url=base_url, login_or_token="ghp_XXX")
 
     xlsx = get_xlsx_obj()
-    #orgs = get_orgs_list(g)
-    # Instead of iterating orgs, we know that we need only "uuc".
-    # The get_orgs_repo_details() has been modified accordingly. 
+    orgs = get_orgs_list(g)
+    # instead of iterating orgs, we know that we need only "uuc"
+    # get_orgs_repo_brances has been modified accordingly 
     #get_orgs_repo_details(g, "uuc", xlsx)
 
-    # Get teams permissions for each repo.
+    # get teams permissions for each repo
     #get_teams_repo_permissions(g, "uuc", xlsx)
 
-    # Get members permissions for each repo.
-    #get_memebers_repo_permissions(g, "uuc", xlsx)
+    # get members permissions for each repo
+    get_memebers_repo_permissions(g, "uuc", xlsx)
 
     save_and_close_xlsx_obj(xlsx)
 
-
 if __name__ == '__main__':
-    start_time = time.time()
     main()
-    print(f"Total execution time: {time.time() - start_time} seconds")
