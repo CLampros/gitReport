1b96755a91d86f4e8663d2bea85170c8a5d4b836
lampros chryssovergis <lampros chryssovergis>
2022-04-23 20:27:50 +0200

    Adding non compliance colours for Repositories. Code refactor.

--- gitReport.py
+++ gitReport.py
@@ -19,7 +19,7 @@ def get_xlsx_obj():
 
 
 def save_and_close_xlsx_obj(workbook):
-    # Save and close xlsx
+    # Save and close xlsx.
     workbook.close()
 
 
@@ -42,10 +42,11 @@ def get_orgs_list(g):
 
 
 def get_orgs_repo_details(g, o, xlsx):
-    # create a worksheet
+    # Create a worksheet.
     worksheet = xlsx.add_worksheet('UUC REPO BRANCHES')
-    # formats
+    # Formats.
     fwrap = xlsx.add_format({
+        'align': 'center',
         'text_wrap': True,
         'valign': 'vcenter'
         })
@@ -63,68 +64,95 @@ def get_orgs_repo_details(g, o, xlsx):
         'font_size': 14
     })
 
+    fnon_compliant = xlsx.add_format({
+        'align': 'center',
+        'valign': 'vcenter',
+        'font_color': 'red',
+    })
+
+    rows = {
+        "repo": 1,
+        "private": 2,
+        "branches": 3,
+        "has_prod": 4,
+        "protected": 5,
+        "has_teams": 6,
+        "author_name": 7,
+        "author_email": 8,
+    }
     worksheet.set_column('B:B', 50, fcenter)
     worksheet.set_column('C:C', 10, fwrap)
     worksheet.set_column('D:D', 30, fwrap)
-    worksheet.set_column('E:E', 10, fwrap)
-    worksheet.set_column('F:F', 10, fwrap)
-    worksheet.set_column('G:G', 30, fwrap)
-    worksheet.set_column('H:H', 50, fwrap)
+    worksheet.set_column('E:E', 15, fwrap)
+    worksheet.set_column('F:F', 15, fwrap)
+    worksheet.set_column('G:G', 15, fwrap)
+    worksheet.set_column('H:H', 30, fwrap)
+    worksheet.set_column('I:I', 50, fwrap)
     
-    # headers
-    worksheet.write(1, 1, "Repo", fheaders)
-    worksheet.write(1, 2, "Private", fheaders)
-    worksheet.write(1, 3, "Branches", fheaders)
-    worksheet.write(1, 4, "Has Prod", fheaders)
-    worksheet.write(1, 5, "Protected", fheaders)
-    worksheet.write(1, 6, "Author name", fheaders)
-    worksheet.write(1, 7, "Author e-mail", fheaders)
+    # Headers.
+    worksheet.write(1, rows["repo"], "Repo", fheaders)
+    worksheet.write(1, rows["private"], "Private", fheaders)
+    worksheet.write(1, rows["branches"], "Branches", fheaders)
+    worksheet.write(1, rows["has_prod"], "Has Prod", fheaders)
+    worksheet.write(1, rows["protected"], "Protected", fheaders)
+    worksheet.write(1, rows["has_teams"], "Has Teams", fheaders)
+    worksheet.write(1, rows["author_name"], "Author name", fheaders)
+    worksheet.write(1, rows["author_email"], "Author e-mail", fheaders)
 
     row = 2
     for org in g.get_user().get_orgs():
         if org.login == o:
             for repo in org.get_repos():
-                # name
-                worksheet.write(row, 1, repo.name)
-
-                # is private?
-                worksheet.write(row, 2, repo.private)
-
-                # brances
+                # Name. If it is not compliant is getting updated with red font color later on.
+                worksheet.write(row, rows["repo"], repo.name)
+                # Is private?
+                worksheet.write(row, rows["private"], repo.private)
+                # Branches.
                 branches = [branch.name for branch in repo.get_branches()]
                 branches_str = "\n".join(branches)
-                worksheet.write(row, 3, branches_str)
-
-                # has prod
+                worksheet.write(row, rows["branches"], branches_str)
+                # Has prod?
                 has_prod = 'prod' in branches
-                worksheet.write(row, 4, has_prod)
+                worksheet.write(row, rows["has_prod"], has_prod)
 
-                # last commit on prod branch (if exists)
+                # Has teams?
+                teams = repo.get_teams()
+
+                if teams:
+                    worksheet.write(row, rows["has_teams"], "TRUE")
+                else:
+                    worksheet.write(row, rows["has_teams"], "FALSE")
+                    worksheet.write(row, rows["repo"], repo.name, fnon_compliant)
+
+                # Last commit on prod branch (if exists).
                 if has_prod:
-                    # is protected?
-                    isProtected = repo.get_branch('prod').protected
-                    worksheet.write(row, 5, isProtected)
-                    
-                    # commiter_name & commiter_email
+                    # Is protected?
+                    protected = repo.get_branch('prod').protected
+                    worksheet.write(row, rows["protected"], protected)
+                    if not protected:
+                        worksheet.write(row, rows["repo"], repo.name, fnon_compliant)
+                        
+                    # Commiter_name & commiter_email.
                     try:
                         commiter_name = repo.get_branch('prod').commit.author.name
                         commiter_email = repo.get_branch('prod').commit.author.email
-                        worksheet.write(row, 6, commiter_name)
-                        worksheet.write(row, 7, commiter_email)
+                        worksheet.write(row, rows["author_name"], commiter_name)
+                        worksheet.write(row, rows["author_email"], commiter_email)
                     except AttributeError:
-                        worksheet.write(row, 6, 'No commits')
-                        worksheet.write(row, 7, 'No commits')
+                        worksheet.write(row, rows["author_name"], 'No commits')
+                        worksheet.write(row, rows["author_email"], 'No commits')
                 else:
-                    worksheet.write(row, 5, 'No prod')
-                    worksheet.write(row, 6, 'No prod')
-                    worksheet.write(row, 7, 'No prod')
+                    worksheet.write(row, rows["repo"], repo.name, fnon_compliant)
+                    worksheet.write(row, rows["protected"], 'No prod')
+                    worksheet.write(row, rows["author_name"], 'No prod')
+                    worksheet.write(row, rows["author_email"], 'No prod')
                 row += 1
 
 
 def get_teams_repo_permissions(g, o, xlsx):
-    # create a worksheet
+    # Create a worksheet.
     worksheet = xlsx.add_worksheet('Teams Permissions')
-    # formats
+    # Formats.
     fwrap = xlsx.add_format({
         'text_wrap': True,
         'valign': 'vcenter'
@@ -143,39 +171,44 @@ def get_teams_repo_permissions(g, o, xlsx):
         'font_size': 14
     })
 
+    rows = {
+        "team": 1,
+        "repo": 2,
+        "permissions": 3,
+    }
     worksheet.set_column('B:B', 30, fcenter)
     worksheet.set_column('C:C', 30, fcenter)
-    worksheet.set_column('D:D', 50, fwrap)
+    worksheet.set_column('D:D', 20, fwrap)
     
-    # headers
-    worksheet.write(1, 1, "Team", fheaders)
-    worksheet.write(1, 2, "Repo", fheaders)
-    worksheet.write(1, 3, "Permissions", fheaders)
+    # Headers.
+    worksheet.write(1, rows["team"], "Team", fheaders)
+    worksheet.write(1, rows["repo"], "Repo", fheaders)
+    worksheet.write(1, rows["permissions"], "Permissions", fheaders)
 
     for org in g.get_user().get_orgs():
         if org.login == o:
             row = 2
             for team in org.get_teams():
                 for repo in team.get_repos():
-                    # team
-                    worksheet.write(row, 1, team.name)
+                    # Team.
+                    worksheet.write(row, rows["team"], team.name)
 
-                    # repo
-                    worksheet.write(row, 2, repo.name)
+                    # Repo.
+                    worksheet.write(row, rows["repo"], repo.name)
 
-                    # permissions
+                    # Permissions.
                     json_permissions = json.dumps(team.get_repo_permission(repo).raw_data, indent = 4)
                     json_permissions = json_permissions.replace('{', '')
                     json_permissions = json_permissions.replace('}', '')
-                    worksheet.write(row, 3, json_permissions)
+                    worksheet.write(row, rows["permissions"], json_permissions)
 
                     row += 1
 
 
 def get_memebers_repo_permissions(g, o, xlsx):
-    # create a worksheet
+    # Create a worksheet.
     worksheet = xlsx.add_worksheet('Members Permissions')
-    # formats
+    # Formats.
     fwrap = xlsx.add_format({
         'text_wrap': True,
         'valign': 'vcenter'
@@ -198,39 +231,48 @@ def get_memebers_repo_permissions(g, o, xlsx):
     worksheet.set_column('C:C', 30, fcenter)
     worksheet.set_column('D:D', 15, fcenter)
     worksheet.set_column('E:E', 25, fwrap)
+
+    rows = {
+        "repo": 1,
+        "collaborators": 2,
+        "repo_admin": 3,
+        "permissions": 4,
+    }
     
-    # headers
-    worksheet.write(1, 1, "Repo", fheaders)
-    worksheet.write(1, 2, "Collaborator", fheaders)
-    worksheet.write(1, 3, "Repo admin", fheaders)
-    worksheet.write(1, 4, "Permissions", fheaders)
+    # Headers.
+    worksheet.write(1, rows["repo"], "Repo", fheaders)
+    worksheet.write(1, rows["collaborators"], "Collaborator", fheaders)
+    worksheet.write(1, rows["repo_admin"], "Repo admin", fheaders)
+    worksheet.write(1, rows["permissions"], "Permissions", fheaders)
 
     for org in g.get_user().get_orgs():
         if org.login == o:
             row = 2
             for repo in org.get_repos():
-                worksheet.write(row, 1, repo.name)
+                worksheet.write(row, rows["repo"], repo.name)
                 for collaborator in repo.get_collaborators():
-                    # Sleep if no more requests are available until they have been replenished
-                    print(f"{row}")
+                    # Sleep if no more requests are available until they have been replenished.
                     sleep_if_core_rate_limit_reached(g)
-                    # permissions
+                    # Permissions.
                     # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html
                     # https://docs.github.com/en/rest/reference/collaborators#get-repository-permissions-for-a-user
                     # https://docs.github.com/en/enterprise-server@3.2/organizations/managing-access-to-your-organizations-repositories/viewing-people-with-access-to-your-repository
                     json_permissions = json.dumps(collaborator.permissions.raw_data, indent = 4)
                     json_permissions = json_permissions.replace('{', '')
                     json_permissions = json_permissions.replace('}', '')
-                    worksheet.write(row, 2, collaborator.login)
-                    worksheet.write(row, 3, repo.get_collaborator_permission(collaborator))
-                    worksheet.write(row, 4, json_permissions)
+                    worksheet.write(row, rows["collaborators"], collaborator.login)
+                    worksheet.write(row, rows["repo_admin"], repo.get_collaborator_permission(collaborator))
+                    worksheet.write(row, rows["permissions"], json_permissions)
                     row += 1
 
 
 def main():
     # Setup.
     load_dotenv()
-    GIT_TOKEN = os.environ.get("GIT_TOKEN")
+    GIT_TOKEN = os.environ.get("GIT_TOKEN", None)
+    if GIT_TOKEN is None:
+        print("No access token provided")
+        exit(1)
     hostname = "github.kyndryl.net"
     base_url = f"https://{hostname}/api/v3"
     g = Github(base_url=base_url, login_or_token=GIT_TOKEN)
@@ -247,10 +289,10 @@ def main():
     #orgs = get_orgs_list(g)
     # Instead of iterating orgs, we know that we need only "uuc".
     # The get_orgs_repo_details() has been modified accordingly. 
-    #get_orgs_repo_details(g, "uuc", xlsx)
+    get_orgs_repo_details(g, "uuc", xlsx)
 
     # Get teams permissions for each repo.
-    #get_teams_repo_permissions(g, "uuc", xlsx)
+    get_teams_repo_permissions(g, "uuc", xlsx)
 
     # Get members permissions for each repo.
     #get_memebers_repo_permissions(g, "uuc", xlsx)
